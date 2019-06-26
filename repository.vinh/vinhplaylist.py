1f33b044f3baaf60729a3903b048a5397559f821
vinhcomp <vinhphuc81@gmail.com>
2019-06-25 23:20:27 -0700

    Update vinhplaylist.py

--- repository.vinh/vinhplaylist.py
+++ repository.vinh/vinhplaylist.py
@@ -957,7 +957,7 @@ def vonglap(url, n):
 					return vonglap(url=url, n=n+1)
 	else:
 		url = None
-		notice('Đã Thử Nhiều Lần', '[COLOR yellow]Đài Hiện Không Mở Được.[/COLOR]', '[COLOR yellow]Đợi Vinh Sửa![/COLOR]')
+		notice('Đã Thử Nhiều Lần', '[COLOR yellow]Đài Hiện Tại Không Mở Được.[/COLOR]', '[COLOR yellow]Đợi Vinh Sửa![/COLOR]')
 	return url
 
 
