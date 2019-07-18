4b1d0f81a92388350e77aa4cc7b57bedd96f39e2
vinhcomp <vinhphuc81@gmail.com>
2019-07-18 14:43:32 -0700

    Update vinhplaylist.py

--- repository.vinh/vinhplaylist.py
+++ repository.vinh/vinhplaylist.py
@@ -244,6 +244,62 @@ def M3UToItems(url_path=""):
 		items = items + [nextitem]
 		return items
 
+	elif url_path.startswith('http://worldkodi.com'):
+		content = requests.get(url_path, headers=headers2).content
+		if '<dir>' in content:
+			item_re = '<dir>.*?\n<title>(.*?)</title>.*?\n<link>(.*?)</link>.*?\n<thumbnail>(.*?)</thumbnail>.*?\n.*?\n</dir>'
+			matchs = re.compile(item_re).findall(content)
+			items = []
+			for label, path, thumb in matchs:
+				path = pluginrootpath+"/m3u/"+urllib.quote_plus(path)
+				item = {
+					"label": label.strip(),
+					"thumbnail": thumb.strip(),
+ 					"path": path.strip(),
+				}
+				items += [item]
+			return items
+		if '<item>' in content:
+			if '<sublink>' in content:
+				content = "".join(content.splitlines())
+				#Limit 4 sublinks
+				item_re = '<title>(.*?)</title><link><sublink>(.*?)</sublink>.*?</link><thumbnail>(.*?)</thumbnail>'
+				item_re2 = '<title>(.*?)</title><link><sublink>.*?</sublink><sublink>(.*?)</sublink>.*?</link><thumbnail>(.*?)</thumbnail>'
+				item_re3 = '<title>(.*?)</title><link><sublink>.*?</sublink><sublink>.*?</sublink><sublink>(.*?)</sublink>.*?</link><thumbnail>(.*?)</thumbnail>'
+				item_re4 = '<title>(.*?)</title><link><sublink>.*?</sublink><sublink>.*?</sublink><sublink>.*?</sublink><sublink>(.*?)</sublink>.*?</link><thumbnail>(.*?)</thumbnail>'
+				matchs = re.compile(item_re).findall(content)
+				matchs2 = re.compile(item_re2).findall(content)
+				matchs3 = re.compile(item_re3).findall(content)
+				matchs4 = re.compile(item_re4).findall(content)
+				matchsall=matchs+matchs2+matchs3+matchs4
+				items = []
+				for label, path, thumb in matchsall:
+					item = {
+						"label": label.strip(),
+						"thumbnail": thumb.strip(),
+						"path": path.strip(),
+					}
+					item["path"] = pluginrootpath + "/play/" + urllib.quote_plus(item["path"])
+					item["is_playable"] = True
+					item["info"] = {"type": "video"}
+					items += [item]
+				return items
+			else:
+				item_re = '<item>.*?\n<title>(.*?)</title>.*?\n<link>(.*?)</link>.*?\n<thumbnail>(.*?)</thumbnail>.*?\n.*?\n</item>'
+				matchs = re.compile(item_re).findall(content)
+				items = []
+				for label, path, thumb in matchs:
+					item = {
+						"label": label.strip(),
+						"thumbnail": thumb.strip(),
+ 						"path": path.strip(),
+					}
+					item["path"] = pluginrootpath + "/play/" + urllib.quote_plus(item["path"])
+					item["is_playable"] = True
+					item["info"] = {"type": "video"}
+					items += [item]
+				return items
+
 	else:
 		item_re = '\#EXTINF(.*?,)(.*?)\n(.*?)\n'
 		(resp, content) = http.request(
