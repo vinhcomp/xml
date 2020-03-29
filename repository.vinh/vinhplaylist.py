a4ef23583e6c65a505f98fab75583719b3bc13b0
vinhcomp <vinhphuc81@gmail.com>
2020-03-29 16:45:45 -0700

    Update vinhplaylist.py

--- repository.vinh/vinhplaylist.py
+++ repository.vinh/vinhplaylist.py
@@ -667,31 +667,31 @@ def getItems(url_path="0", tq="select A,B,C,D,E"):
 				item["is_playable"] = True
 				item["info"] = {"type": "video"}
 				item["path"] = pluginrootpath + "/play/" + urllib.quote_plus(item["path"])
-			#elif "youtube.com/channel" in item["path"]:
-				## https://www.youtube.com/channel/UC-9-kyTW8ZkZNDHQJ6FgpwQ
-				#yt_route = "ytcp" if "playlists" in item["path"] else "ytc"
-				#yt_cid = re.compile("youtube.com/channel/(.+?)$").findall(item["path"])[0]
-				#item["path"] = "plugin://plugin.video.kodi4vn.launcher/%s/%s/" % (
-					#yt_route, yt_cid)
-				#item["path"] = item["path"].replace("/playlists", "")
-			#elif "youtube.com/playlist" in item["path"]:
-				## https://www.youtube.com/playlist?list=PLFgquLnL59alCl_2TQvOiD5Vgm1hCaGSI
-				#yt_pid = re.compile("list=(.+?)$").findall(item["path"])[0]
-				#item["path"] = "plugin://plugin.video.kodi4vn.launcher/ytp/%s/" % yt_pid
-			#elif any(ext in item["path"] for ext in [".png", ".jpg", ".bmp", ".jpeg"]):
-				#item["path"] = "plugin://plugin.video.kodi4vn.launcher/showimage/%s/" % urllib.quote_plus(
-					#item["path"])
-			
 			elif "youtube.com/channel" in item["path"]:
 				# https://www.youtube.com/channel/UC-9-kyTW8ZkZNDHQJ6FgpwQ
 				yt_route = "ytcp" if "playlists" in item["path"] else "ytc"
 				yt_cid = re.compile("youtube.com/channel/(.+?)$").findall(item["path"])[0]
-				item["path"] = "plugin://plugin.video.youtube/channel/%s/" % yt_cid
-
+				item["path"] = "plugin://plugin.video.kodi4vn.launcher/%s/%s/" % (
+					yt_route, yt_cid)
+				item["path"] = item["path"].replace("/playlists", "")
 			elif "youtube.com/playlist" in item["path"]:
 				# https://www.youtube.com/playlist?list=PLFgquLnL59alCl_2TQvOiD5Vgm1hCaGSI
 				yt_pid = re.compile("list=(.+?)$").findall(item["path"])[0]
-				item["path"] = "plugin://plugin.video.youtube/playlist/%s/" % yt_pid
+				item["path"] = "plugin://plugin.video.kodi4vn.launcher/ytp/%s/" % yt_pid
+			elif any(ext in item["path"] for ext in [".png", ".jpg", ".bmp", ".jpeg"]):
+				item["path"] = "plugin://plugin.video.kodi4vn.launcher/showimage/%s/" % urllib.quote_plus(
+					item["path"])
+			
+			#elif "youtube.com/channel" in item["path"]:
+				## https://www.youtube.com/channel/UC-9-kyTW8ZkZNDHQJ6FgpwQ
+				#yt_route = "ytcp" if "playlists" in item["path"] else "ytc"
+				#yt_cid = re.compile("youtube.com/channel/(.+?)$").findall(item["path"])[0]
+				#item["path"] = "plugin://plugin.video.youtube/channel/%s/" % yt_cid
+
+			#elif "youtube.com/playlist" in item["path"]:
+				## https://www.youtube.com/playlist?list=PLFgquLnL59alCl_2TQvOiD5Vgm1hCaGSI
+				#yt_pid = re.compile("list=(.+?)$").findall(item["path"])[0]
+				#item["path"] = "plugin://plugin.video.youtube/playlist/%s/" % yt_pid
 
 			elif item["path"].startswith('ytsearch'):
 				#ytsearch/nhu loan
