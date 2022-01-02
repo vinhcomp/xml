51f5211877bad4c53716b7bd9f011c4ce427431f
vinhcomp <vinhphuc81@gmail.com>
2022-01-01 16:34:56 -0800

    Update vinhmovies.py

--- repository.vinh/vinhmovies.py
+++ repository.vinh/vinhmovies.py
@@ -306,7 +306,7 @@ def Layer2ToItems(url_path=""):
 		return items
 
 	#elif url_path.startswith('http://dl.upload10') or url_path.startswith('http://dl2.upload10'): #from http://builds.kodiuk boxsets
-	elif any(url_path.startswith(domain) for domain in ['http://dl.upload10', 'http://dl2.upload10', 'http://dl2.uploadzone', 'http://perserver.ir/']):
+	elif any(url_path.startswith(domain) for domain in ['http://dl.upload10', 'http://dl2.upload10', 'http://dl2.uploadzone', 'http://perserver.ir/', 'https://movies.encrypticmh', 'http://rmeyer.comelitdns.com']): #Movies 1 Click
 		content = requests.get(url_path, headers=headers2).content
 		#matchs = re.findall('<a href="(.*?)">(.*?)<',content)[:]
 		item_re = '<a href="(.*?)">(.*?)<'
@@ -326,7 +326,7 @@ def Layer2ToItems(url_path=""):
 				item["path"] = pluginrootpath + "/play/" + urllib.quote_plus(item["path"])
 				item["is_playable"] = True
 				item["info"] = {"type": "video"}
-			items += [item]	
+			items += [item]
 		return items
 
 	elif url_path.startswith('http://ftp.alphamediazone.com'):
@@ -1008,7 +1008,7 @@ def M3UToItems(url_path=""):
 		return items
 
 	#elif url_path.startswith('http://crackstreams.com') or url_path.startswith('http://nbastreams.xyz') or ('http://crackstreams.is'):
-	elif url_path.startswith('http://crackstreams.is') or url_path.startswith('http://crackstreams.net') or url_path.startswith('http://hd.crackstreams.net') or url_path.startswith('http://new.crackstreams.net') or url_path.startswith('http://ww3.crackstreams.net/'):
+	elif url_path.startswith('http://crackstreams.is') or url_path.startswith('http://crackstreams.net') or url_path.startswith('http://hd.crackstreams.net') or url_path.startswith('http://new.crackstreams.net') or url_path.startswith('http://ww3.crackstreams.net'):
 		content = requests.get(url_path, headers=headers2).content
 		#content = "".join(content.splitlines())
 		#item_re = "<a href='(.*?)'.*?<img src='(.*?)'.*?media-heading'>(.*?)<.*?<p>(.*?)</p>"
@@ -2192,7 +2192,7 @@ def play_url(url, title=""):
 	#		linkstream = base64.b64decode(link2)+'|User-Agent=iPad&Referer='+link
 		plugin.set_resolved_url(linkstream, subtitles=vsub)
 
-	elif url.startswith('http://crackstreams.net') or url.startswith('http://hd.crackstreams.net') or url.startswith('http://new.crackstreams.net') or url.startswith('http://ww3.crackstreams.net'):
+	elif any(url.startswith(domain) for domain in ['http://crackstreams.net', 'http://hd.crackstreams.net', 'http://new.crackstreams.net', 'http://ww3.crackstreams.net']):
 		source = requests.get(url, headers=headers2).text
 		link = re.findall('<iframe.*?src="(.*?)"', source)[0]
 		source2 = requests.get(link, headers=headers2).text
